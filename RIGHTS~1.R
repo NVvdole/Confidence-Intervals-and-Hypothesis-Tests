55646d212f54dc88a2a78f9cb234c51808e16104
V Dole <102748394+NVvdole@users.noreply.github.com>
2022-04-07 10:54:39 -0500

    Add files via upload

---
+++ RIGHTS~1.R
@@ -0,0 +1,27 @@
+# File Name: RightSidedCorrelationTest.R
+# Author: Verginia Mae Dole
+# Date Created: 4/7/2022
+
+{
+  n <- as.integer(readline(prompt = "Enter n: "))
+  while (n <= 0) {
+    n <- as.integer(readline(prompt = "Enter n: "))
+  }
+  
+  x <- c()
+  for (i in 1:n) {
+    x[i] <- as.double(readline(prompt = paste("Enter x", as.character(i), " : ", sep = "")))
+  }
+  
+  y <- c()
+  for (i in 1:n) {
+    y[i] <- as.double(readline(prompt = paste("Enter y", as.character(i), " : ", sep = "")))
+  }
+  
+  CL <- as.double(readline(prompt = "Enter CL: "))
+  while (CL <= 0.0 || CL >= 1.0) {
+    CL <- as.double(readline(prompt = "Enter CL: "))
+  }
+  
+  cor.test(x = x, y = y, alternative = "less", method = "pearson", conf.level = CL)
+}
