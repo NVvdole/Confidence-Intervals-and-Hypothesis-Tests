cff699bfc88134b17731c7518ea9ded73281de1f
V Dole <102748394+NVvdole@users.noreply.github.com>
2022-04-13 16:15:31 -0500

    Add files via upload

---
+++ GOFChiSquareTest.R
@@ -0,0 +1,30 @@
+# File Name: GOFChiSquareTest.R
+# Author: Verginia Mae Dole
+# Date Created: 4/6/2022
+
+{
+  n <- as.integer(readline(prompt = "Enter n: "))
+  while (n <= 0) {
+    n <- as.integer(readline(prompt = "Enter n: "))
+  }
+  
+  O <- c()
+  for (i in 1:n) {
+    O[i] <- as.double(readline(prompt = paste("Enter O", as.character(i), " : ", sep = "")))
+    while (O[i] <= 0.0) {
+      O[i] <- as.double(readline(prompt = paste("Enter O", as.character(i), " : ", sep = "")))
+    }
+  }
+  
+  E <- c()
+  for (i in 1:n) {
+    E[i] <- as.double(readline(prompt = paste("Enter E", as.character(i), " : ", sep = "")))
+    while (E[i] <= 0.0) {
+      E[i] <- as.double(readline(prompt = paste("Enter E", as.character(i), " : ", sep = "")))
+    }
+  }
+  
+  p <- E / sum(O)
+  
+  chisq.test(x = O, p = p)
+}
