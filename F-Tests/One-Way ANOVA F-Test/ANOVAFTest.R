4071e9e7f0a1070806e022d5fd1c1bb4464dc925
V Dole <102748394+NVvdole@users.noreply.github.com>
2022-04-15 10:35:41 -0500

    Update ANOVAFTest.R

--- F-Tests/One-Way ANOVA F-Test/ANOVAFTest.R
+++ F-Tests/One-Way ANOVA F-Test/ANOVAFTest.R
@@ -9,17 +9,17 @@
   }
   
   x <- c()
-  i = 0
-  j = 1
+  i = 1
+  j = 0
   for (k in 1:(3 * n)) {
-    i <- i + 1
+    j <- j + 1
     x[k] <- as.double(readline(prompt = paste("Enter x", as.character(i), as.character(j), ": ", sep = "")))
     while (x[k] <= 0.0) {
       x[k] <- as.double(readline(prompt = paste("Enter x", as.character(i), as.character(j), ": ", sep = "")))
     }
-    if (i == n) {
-      i = 0
-      j <- j + 1
+    if (j == n) {
+      j = 0
+      i <- i + 1
     }
   }
   
